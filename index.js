6ffd50612d485da1f9af6c0223558ae444362d6c
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-21 17:07:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('This is the starting point');
+const express = require('express');
+const app = express();
+const connectDB = require('./db');
+const bodyParser = require('body-parser');
+
+// Connect to the database
+connectDB();
+
+app.use(bodyParser.json());
+
+// Import routes
+const batteryRoutes = require('./routes/batteryRoutes');
+
+// Use routes
+app.use('/batteries', batteryRoutes);
+
+const PORT = process.env.PORT || 3001;
+const server = app.listen(PORT, () => {
+  console.log(`Server started running on port ${PORT}`);
+});
+
+module.exports = {app, server};
