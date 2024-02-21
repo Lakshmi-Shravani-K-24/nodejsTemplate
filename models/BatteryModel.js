038001f74d6fd2167e6d569173bb75cd6ef5a04a
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-21 17:05:03 +0530

    Create BatteryModel.js

---
+++ models/BatteryModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const BatterySchema = new mongoose.Schema({
+  batteryname: {
+    type: String,
+    required: true,
+  },
+  temperature: {
+    type: Number,
+    required: true,
+  },
+  soc: {
+    type: Number,
+    required: true,
+  },
+  chargerate: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+    min: 2,
+    max: 50,
+  },
+}, {timestamps: true});
+
+const Battery = mongoose.model('Battery', BatterySchema);
+
+module.exports = Battery;
