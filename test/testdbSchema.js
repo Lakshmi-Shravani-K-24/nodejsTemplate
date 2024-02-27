79c72cf997df712e7896836aa66473da8fa3b9ea
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-27 19:01:48 +0530

    Update testdbSchema.js

--- test/testdbSchema.js
+++ test/testdbSchema.js
@@ -1,6 +1,5 @@
 const assert = require('assert');
 const Battery = require('../BatterySchema');
-const {connect} = require('../dbconnection.js');
 
 describe('Battery Schema Validation', () => {
   const testCases = [
@@ -80,40 +79,5 @@ describe('Battery Schema Validation', () => {
 
     const result = await battery.validate();
     assert.strictEqual(result, undefined);
-  });
-
-  it('should ensure batteryId is unique', async () => {
-    before(async () => {
-    await connect(); // Connect to the in-memory database before running CRUD tests
-  });
-    const batteryData1 = {
-      batteryId: 'test123',
-      batteryname: 'Test Battery 1',
-      temperature: 25,
-      soc: 50,
-      chargerate: 10,
-    };
-
-    const batteryData2 = {
-      batteryId: 'test123',
-      batteryname: 'Test Battery 2',
-      temperature: 30,
-      soc: 60,
-      chargerate: 12,
-    };
-
-    const battery1 = new Battery(batteryData1);
-    await battery1.save();
-
-    let err;
-    const battery2 = new Battery(batteryData2);
-    try {
-      await battery2.save();
-    } catch (error) {
-      err = error;
-    }
-
-    assert.strictEqual(err.name, 'MongoError');
-    assert.strictEqual(err.code, 11000); // MongoDB duplicate key error code
   });
 });
