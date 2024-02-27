d5441d7371903b478494d15872c98902446dd9d0
Lakshmi Shravani K <132111824+Lakshmi-Shravani-K-24@users.noreply.github.com>
2024-02-27 15:24:45 +0530

    Update testdbErrors.js

--- test/testdbErrors.js
+++ test/testdbErrors.js
@@ -1,46 +1,51 @@
+/* eslint-disable no-empty */
 /* eslint-disable max-len */
-const assert = require('assert');
+// const assert = require('assert');
+const {expect} = require('chai');
+
 const {createBattery, findBatteryById, updateBattery, deleteBattery} = require('../index.js');
 
 describe('Error Handling in Battery Service', () => {
-  it('should throw error for createBattery', async () => {
+  it('should throw a ValidationError for empty batteryData', async () => {
     const batteryData = {}; // Empty data to intentionally cause an error
 
     try {
       await createBattery(batteryData);
     } catch (error) {
-      assert.strictEqual(error.message, 'Failed to create battery: Battery validation failed: chargerate: Path `chargerate` is required., soc: Path `soc` is required., temperature: Path `temperature` is required., batteryname: Path `batteryname` is required., batteryId: Path `batteryId` is required.' );
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.name).to.equal('Error');
+      expect(error.message).to.equal('Battery data is required');
     }
   });
 
-  it('should throw error for findBatteryById', async () => {
-    const batteryId = 'invalidId'; // Invalid ID to intentionally cause an error
-
+  it('should throw an error when batteryId is not provided', async () => {
+    const batteryId = ''; // Empty ID to intentionally cause an error
     try {
       await findBatteryById(batteryId);
     } catch (error) {
-      assert.strictEqual(error.message, 'Failed to find battery: Client must be connected before running operations');
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal('Battery ID is required');
     }
   });
 
-  it('should throw error for updateBattery', async () => {
-    const batteryId = 'invalidId'; // Invalid ID to intentionally cause an error
+  it('should throw error for updateBattery when batteryId is invalid', async () => {
+    const batteryId = '';
     const updateData = {temperature: 30}; // Arbitrary update data
-
     try {
       await updateBattery(batteryId, updateData);
     } catch (error) {
-      assert.strictEqual(error.message, 'Failed to update battery: Client must be connected before running operations');
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal('Battery ID is required');
     }
   });
 
-  it('should throw error for deleteBattery', async () => {
-    const batteryId = 'invalidId'; // Invalid ID to intentionally cause an error
-
+  it('should throw error for deleteBattery when batteryId is invalid', async () => {
+    const batteryId = ''; // Empty ID to intentionally cause an error
     try {
       await deleteBattery(batteryId);
     } catch (error) {
-      assert.strictEqual(error.message, 'Failed to delete battery: Client must be connected before running operations');
+      expect(error).to.be.an.instanceOf(Error);
+      expect(error.message).to.equal('Battery ID is required');
     }
   });
 });
